cf408fc2a5bda3b928621bfdc2653784c1c8b1b4
dryjorse <kozoevabdulazim@gmail.com>
2023-03-27 10:46:39 +0300

    hash

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 
@@ -10,9 +10,9 @@ import store from './store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Provider store={store}>
-      <BrowserRouter >
+      <HashRouter hashType="hashbang">
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
 );
 
